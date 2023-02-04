75615e7020c9ef1dd02a98cd5ebc300bb273b07a
Fernando Montoya <122655759+montoya150621@users.noreply.github.com>
2023-02-04 10:01:46 -0600

    Add files via upload

---
+++ 04 - Operadores.js
@@ -0,0 +1,15 @@
+// Tipos de operadores
+
+// Operadores Lógicos 
+
+!false // Operador not nega todo
+true 
+false
+a && b // Funciona cuando este y este son verdaderos
+true || false // operador or, validacion cuando este o este son verdaderos
+
+
+// Operadores de comparacion
+= // asignacion
+3 == '3' // comparacion
+3 === '3' // es estrictamente igual 
